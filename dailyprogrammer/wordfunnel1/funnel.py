acdcf40f9b35c93c3b9a8340d510e88d81b42210
David Woo <david.woo82@gmail.com>
2018-09-17 15:19:14 -0500

    Style update.

--- dailyprogrammer/wordfunnel1/funnel.py
+++ dailyprogrammer/wordfunnel1/funnel.py
@@ -1,6 +1,8 @@
 import sys
 
+
 class WordFunnel:
+
     def __init__(self, word_list_filename):
         self.wordlist = self.parse_word_list(word_list_filename)
 
@@ -11,8 +13,7 @@ class WordFunnel:
                 result.append(line.strip().lower())
         return result
 
-    @staticmethod
-    def is_funnel(first, second):
+    def is_funnel(self, first, second):
         '''
         Determine if the second word can be made by removing one letter
         from the first word without changing the order of the letters
@@ -29,7 +30,7 @@ class WordFunnel:
                 return True
 
         return False
-    
+
     def find_list(self, given):
         '''
         Determine list of words that can be made by removing one letter
@@ -40,7 +41,7 @@ class WordFunnel:
             if self.is_funnel(given, word):
                 result.add(word)
         return list(result)
-    
+
     def funnel_length(self, given):
         '''
         Determine funnel lengths (Word Funnel challenge, part 2)
@@ -55,6 +56,7 @@ class WordFunnel:
                 lengths.append(self.funnel_length(w))
             return 1 + max(lengths)
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('Usage: funnel.py <path to word list>')
@@ -63,14 +65,14 @@ if __name__ == "__main__":
     word_list_filename = sys.argv[1]
 
     wf = WordFunnel(word_list_filename)
-    
+
     assert wf.is_funnel("leave", "eave")
     assert wf.is_funnel("reset", "rest")
     assert wf.is_funnel("dragoon", "dragon")
     assert not wf.is_funnel("eave", "leave")
     assert not wf.is_funnel("sleet", "lets")
     assert not wf.is_funnel("skiff", "ski")
-    
+
     print wf.find_list("dragoon")  #  => ["dragon"]
     print wf.find_list("boats")  # => ["oats", "bats", "bots", "boas", "boat"]
     print wf.find_list("affidavit")  # => []
